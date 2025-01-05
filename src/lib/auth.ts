6163b6e6d2aaaa5411c9772d1008fa505b3cc485
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-05 01:19:29 -0300

    Update package dependencies and modify Prisma schema for User model
    - Added @types/bcrypt as a development dependency in package.json and package-lock.json.
    - Updated User model in schema.prisma to enforce unique email and required password fields.

---
+++ src/lib/auth.ts
@@ -0,0 +1,78 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "./prisma";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      email?: string | null;
+      name?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials || !credentials.password) {
+          throw new Error("Email e senha são obrigatórios");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (
+          !user ||
+          !(await bcrypt.compare(credentials.password, user.password))
+        ) {
+          throw new Error("Credenciais inválidas");
+        }
+
+        return { id: user.id, email: user.email, name: user.name };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user = { email: token.email, name: token.name, id: token.id };
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+  },
+};
