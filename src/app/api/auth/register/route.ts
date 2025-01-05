d1fa1ae222fc4506aca37d1dfe1bf8fc65be0502
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-05 01:29:11 -0300

    Enhance User model validation and add development dependencies for improved security

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import  bcrypt  from 'bcrypt';
+import prisma from "@/lib/prisma"
+import { registerSchema } from "@/lib/validators"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const data = registerSchema.parse(body)
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: data.email
+      }
+    })
+
+    if (existingUser) {
+      return NextResponse.json(
+        {error: 'usuário ja existe'},
+        {status: 400}
+      )
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+        name: data.name
+      }
+    })
+
+    return NextResponse.json({user})
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: error.message || 'Erro ao registrar' },
+        { status: 400 }
+      );
+    }
+  }
+}
---
+++ src/lib/validators.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().optional(),
+})
+
+export type RegisterFormData = z.infer<typeof registerSchema>
