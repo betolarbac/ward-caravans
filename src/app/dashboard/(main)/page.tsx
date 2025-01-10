d27cf072616aba0dfa309bf2b158692bc8487d37
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-10 00:56:24 -0300

    feat: refactor Dashboard component to fetch caravan data asynchronously

--- src/app/dashboard/(main)/page.tsx
+++ src/app/dashboard/(main)/page.tsx
@@ -2,155 +2,39 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Header from "../_components/header";
 import Link from "next/link";
 import { Calendar, School, User } from "lucide-react";
+import { getWardCaravans } from "../ward-caravans/actions";
 
-const caravans = [
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-    members: [
-      {
-        id: "1",
-        name: "roberto",
-      },
-      {
-        id: "2",
-        name: "roberto 1",
-      },
-    ],
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-  {
-    name: "Caravana ala antares",
-    data: "2024-03-20",
-    vagas: 45,
-    ward: {
-      id: "1",
-      name: "Ala Antares",
-    },
-  },
-];
 
-export default function Dashboard() {
+export default async function Dashboard() {
+
+  const caravans = await getWardCaravans()
+
   return (
     <div className="max-w-[1200px] mx-auto">
       <Header title="Caravanas da Estaca 2025" />
       <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 min-[1440px]:grid-cols-4">
-        {caravans.map((card, index) => (
-          <Link href={"/"} key={index}>
+        {caravans.map((caravans ) => (
+          <Link href={`/dashboard/ward-caravans/${caravans.id}`} key={caravans.id}>
             <Card>
               <CardHeader>
-                <CardTitle>{card.name}</CardTitle>
+                <CardTitle>{caravans.name}</CardTitle>
               </CardHeader>
 
               <CardContent>
                 <div className="flex justify-between items-center mb-2">
                   <span className="text-sm text-muted-foreground flex items-center">
                     <School className="w-4 h-4 mr-1" />
-                    {card.ward.name}
+                    {caravans.ward?.name}
                   </span>
                   <span className="text-sm text-muted-foreground flex items-center">
                     <Calendar className="w-4 h-4 mr-1" />
-                    {new Date(card.data).toLocaleDateString()}
+                    {caravans.date?.toLocaleDateString("pt-BR")}
                   </span>
                 </div>
 
                 <p className="text-sm font-medium flex items-center">
                   <User className="w-4 h-4 mr-1" />
-                  Vagas: <span className="text-primary ml-1">{card.vagas}</span>
+                  Vagas: <span className="text-primary ml-1">{caravans.Member.length}/{caravans.vacancy}</span>
                 </p>
               </CardContent>
             </Card>
