7e23d797226cb4088360052b4cd50a29fcf371f4
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-10 00:30:56 -0300

    feat: create CaravansPage component to display member details in a table format

---
+++ src/app/dashboard/ward-caravans/[id]/page.tsx
@@ -0,0 +1,63 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import Header from "../../_components/header";
+import { GetCaravansPage } from "./action";
+import MemberCaravans from "./_components/MemberCaravans";
+
+interface CaravanPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function CaravansPage({ params }: CaravanPageProps) {
+  const { id } = await params;
+
+  const caravansMember = await GetCaravansPage(id);
+  return (
+    <div className="max-w-[1200px] mx-auto">
+      <div className="flex justify-between">
+        <Header title={caravansMember?.name as string} />
+        <MemberCaravans id={id as string} />
+      </div>
+
+      <Card>
+        <CardContent className="py-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Nome</TableHead>
+                <TableHead>Ala</TableHead>
+                <TableHead>Cpf</TableHead>
+                <TableHead>Pagou</TableHead>
+                <TableHead>Deletar</TableHead>
+                <TableHead>Editar</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {caravansMember?.Member.map((caravans) => (
+                <TableRow key={caravans.id}>
+                  <TableCell className="font-medium py-4">
+                    {caravans.name}
+                  </TableCell>
+                  <TableCell>{caravans.ward}</TableCell>
+                  <TableCell>{caravans.cpf}</TableCell>
+                  <TableCell>{caravans.pay.toString()}</TableCell>
+                  <TableCell>Deletar</TableCell>
+                  <TableCell>Editar</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
