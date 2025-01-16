0896dfa401a5cd7c2009a170a6cf5d875670719f
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-16 01:55:34 -0300

    feat: nova pagina de login estilizada

--- src/app/auth/_components/loginForm.tsx
+++ src/app/auth/_components/loginForm.tsx
@@ -16,6 +16,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { Loader } from "lucide-react";
 
 export default function LoginForm() {
   const [loading, setLoading] = useState(false);
@@ -52,45 +55,80 @@ export default function LoginForm() {
   }
 
   return (
-    <div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-4 max-w-xs flex-1"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <div className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Bem-vindo de volta</h1>
+                <p className="text-balance text-muted-foreground">
+                Faça login em sua conta
+                </p>
+              </div>
 
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4 max-w-xs flex-1"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-          <Button type="submit" disabled={loading}>
-            {loading ? "Carregando..." : "Entrar"}
-          </Button>
-        </form>
-      </Form>
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit" disabled={loading} className="w-full bg-[#167b9c] hover:bg-[#0f5b7c] transition-colors duration-300 ease-in-out">
+                    {loading ? <Loader className="w-4 h-4 animate-spin" /> : "Entrar"}
+                  </Button>
+                </form>
+              </Form>
+
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative hidden bg-muted md:block">
+            <Image
+              src="https://www.churchofjesuschrist.org/imgs/c4bd3d9a13a611efbf27eeeeac1ed41dbaccc990/full/640%2C/0/default"
+              alt="Image"
+              width={640}
+              height={505}
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
     </div>
   );
 }
--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,9 +2,10 @@ import LoginForm from "../_components/loginForm";
 
 export default function SignInPage() {
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h1 className="text-xl font-bold mb-4">Entrar</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
       <LoginForm />
+      </div>
     </div>
   );
 }
