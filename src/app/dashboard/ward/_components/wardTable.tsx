f10d48ed170aede47a80eee7e8fb6254782da04d
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-01-26 00:07:42 -0300

    feat: Refactor Ward page with advanced table component and improved data handling

---
+++ src/app/dashboard/ward/_components/wardTable.tsx
@@ -0,0 +1,231 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { wardData } from "@/lib/validators";
+import EditModalWard from "./editModalWard";
+import DeleteModalWard from "./deleteModalWard";
+
+type wardDataProps = {
+  data: wardData[];
+};
+
+export function WardTable({ data }: wardDataProps) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const columns: ColumnDef<wardData>[] = [
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Ala
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="capitalize font-medium px-2 py-4">
+          {row.getValue("name")}
+        </div>
+      ),
+    },
+    {
+      accessorKey: "stakeId",
+      header: "Estaca",
+      cell: ({ row }) => (
+        <div className="capitalize font-medium px-2 py-4">
+          {row.getValue("stakeId")}
+        </div>
+      ),
+    },
+    {
+      id: "delete",
+      header: "Deletar",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const data = row.original;
+        return (
+          <DeleteModalWard idWards={data.id as string} name={data.name ?? ""} />
+        );
+      },
+    },
+    {
+      id: "edit",
+      header: "Editar",
+      enableHiding: false,
+      cell: ({ row }) => {
+        const data = row.original;
+        return <EditModalWard name={data.name ?? ""} id={data.id} stakeId="" />;
+      },
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Nome da Ala"
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Sem Caravanas cadastradas
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/dashboard/ward/page.tsx
+++ src/app/dashboard/ward/page.tsx
@@ -1,19 +1,10 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import Header from "../_components/header";
 import { Card, CardContent } from "@/components/ui/card";
 import ModalFormWard from "./_components/modalFormWard";
 import { getWards } from "./action";
-import DeleteModalWard from "./_components/deleteModalWard";
-import EditModalWard from "./_components/editModalWard";
 import UserLoggedIn from "../_components/UserloggedIn/UserloggedIn";
 import { redirect } from "next/navigation";
+import { WardTable } from "./_components/wardTable";
 
 export default async function Ward() {
   const { Stake, role } = await UserLoggedIn();
@@ -32,47 +23,13 @@ export default async function Ward() {
       </div>
       <Card>
         <CardContent className="py-4">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Ala</TableHead>
-                <TableHead>Estaca</TableHead>
-                <TableHead>Deletar</TableHead>
-                <TableHead>Editar</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {ward.length === 0 ? (
-                <TableRow>
-                  <TableCell colSpan={6} className="h-24 text-center">
-                    Nenhuma Ala cadastrada.
-                  </TableCell>
-                </TableRow>
-              ) : (
-                ward.map((ward) => (
-                  <TableRow className="" key={ward.id}>
-                    <TableCell className="font-medium py-4">
-                      {ward.name}
-                    </TableCell>
-                    <TableCell>{ward.stake?.name}</TableCell>
-                    <TableCell>
-                      <DeleteModalWard
-                        idWards={ward.id}
-                        name={ward.name ?? ""}
-                      />
-                    </TableCell>
-                    <TableCell>
-                      <EditModalWard
-                        name={ward.name ?? ""}
-                        id={ward.id}
-                        stakeId=""
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))
-              )}
-            </TableBody>
-          </Table>
+          <WardTable
+            data={ward.map((ward) => ({
+              id: ward.id,
+              name: ward.name ?? "",
+              stakeId: ward.stake?.name ?? "",
+            }))}
+          />
         </CardContent>
       </Card>
     </div>
