8e5ab4bbd25c3a09daa557294b5a18670faa7c97
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2025-02-02 01:21:59 -0300

    feat: Add POST endpoint for creating caravan members with validation

--- src/app/api/caravans/id/route.ts
+++ src/app/api/caravans/id/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import { CaravansMemberSchema } from "@/lib/validators";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_req: NextRequest) {
@@ -24,6 +25,29 @@ export async function GET(_req: NextRequest) {
     return NextResponse.json(
       { error: "Erro ao buscar usuários" },
       { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const validatedData = CaravansMemberSchema.parse(body);
+
+    const memberCaravan = await prisma.member.create({
+      data: validatedData,
+    });
+
+    return NextResponse.json(memberCaravan);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Erro:", error.message);
+    }
+
+    return NextResponse.json(
+      { error: "Erro ao cadastrar usuário" },
+      { status: 400 }
     );
   }
 }
